f1ad550136c6c7690877876183aa777fa25b5fc8
imamrozi <imam.rozi@polinema.ac.id>
2019-09-25 06:02:17 +0700

    ok

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -0,0 +1,38 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.BlueFragment;
+import polinema.ac.id.starterchapter05.fragments.RedFragment;
+
+public class DynamicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic);
+    }
+
+    public void handlerLoadRedFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        //fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+        fragmentTransaction.replace(R.id.fragment,new RedFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    public void handlerLoadBlueFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        //fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_ENTER_MASK);
+        fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+        fragmentTransaction.replace(R.id.fragment,new BlueFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
